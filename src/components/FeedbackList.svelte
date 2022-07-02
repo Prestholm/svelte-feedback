fb9932ab96bb098a82d4f8751c1037a70f66f35e
Victor Prestholm <victor.prestholm@gmail.com>
2022-07-02 15:24:08 +0200

    Svelte-feedback

---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,13 @@
+<script>
+    import {FeedbackStore} from '../stores';
+    import {fade, scale} from 'svelte/transition';
+    import FeedbackItem from './FeedbackItem.svelte';
+
+</script>
+
+{#each $FeedbackStore as fb (fb.id)}
+<div transition:fade >
+  <FeedbackItem item={fb} />
+</div>
+
+{/each}
